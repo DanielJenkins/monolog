757ae5ec2a1756b78cd554a4729d590a7b6ce986
Daniel Jenkins <DanielPatrickJenkins@gmail.com>
2016-01-20 14:46:47 -0800

    Adding gulpfile.js with jade and sass conversion functionality

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var jade = require('gulp-jade');
+
+var app = 'application/app.js';
+
+gulp.task('default', ['jade','sass','watch']);
+
+gulp.task('jade', function() {
+  console.log('Running Jade');
+  gulp.src('./public/**/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./public/html'))
+});
+
+gulp.task('sass', function () {
+  console.log('Running Sass');
+  gulp.src('./public/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./public/**/*.jade', ['jade']);
+  gulp.watch('./public/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('nodemon', function() {
+  nodemon({
+    script: app,
+  }).on('start');
+});
