41da64b8c9fe6e3848eba21137a064eb36c76206
Daniel Jenkins <DanielPatrickJenkins@gmail.com>
2016-01-25 16:51:22 -0800

    Beginning to establsh format for index.html

--- application/app.js
+++ application/app.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.use(express.static('server/public'));
+
+app.use(express.static('application/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname+'/public/html/index.html'));
+});
 
 app.listen(1337, function() {
   console.log('App is running!');
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var jade = require('gulp-jade');
+var nodemon = require('gulp-nodemon');
 
-gulp.task('default', ['jade','sass','watch']);
+gulp.task('default', ['jade','sass','watch','nodemon']);
 
 gulp.task('jade', function() {
   console.log('Running Jade');
-  gulp.src('./public/**/*.jade')
+  gulp.src('./application/public/**/*.jade')
     .pipe(jade())
-    .pipe(gulp.dest('./public/html'))
+    .pipe(gulp.dest('./application/public/html'))
 });
 
 gulp.task('sass', function () {
   console.log('Running Sass');
-  gulp.src('./public/sass/**/*.scss')
+  gulp.src('./application/public/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./application/public/css'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./public/**/*.jade', ['jade']);
-  gulp.watch('./public/sass/**/*.scss', ['sass']);
+  gulp.watch('./application/public/**/*.jade', ['jade']);
+  gulp.watch('./application/public/sass/**/*.scss', ['sass']);
 });
 
 var app = 'application/app.js';
