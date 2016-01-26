e02be3e96506051401509788db568ba3f7217211
Daniel Jenkins <DanielPatrickJenkins@gmail.com>
2016-01-26 14:12:41 -0800

    Adding basic new user form - #4

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,21 @@ gulp.task('default', ['jade','sass','watch','nodemon']);
 
 gulp.task('jade', function() {
   console.log('Running Jade');
-  gulp.src('./application/public/**/*.jade')
+  gulp.src('./build/jade/**/*.jade')
     .pipe(jade())
     .pipe(gulp.dest('./application/public/html'))
 });
 
 gulp.task('sass', function () {
   console.log('Running Sass');
-  gulp.src('./application/public/sass/**/*.scss')
+  gulp.src('./build/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./application/public/css'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./application/public/**/*.jade', ['jade']);
-  gulp.watch('./application/public/sass/**/*.scss', ['sass']);
+  gulp.watch('./build/jade/**/*.jade', ['jade']);
+  gulp.watch('./build/sass/**/*.scss', ['sass']);
 });
 
 var app = 'application/app.js';
