e43ddacf62cadf1612523b7736bb68de4b4ce564
Daniel Jenkins <DanielPatrickJenkins@gmail.com>
2016-01-28 15:52:59 -0800

    Adding navbar to login page

--- application/public/js/all.js
+++ application/public/js/all.js
@@ -6,11 +6,21 @@ app.config(['$routeProvider', function($routeProvider) {
       templateUrl: "../html/login.html",
       controller: "loginpageController",
       controllerAs: "loginpage"
+    })
+    .when("/join", {
+      templateUrl: "../html/join.html",
+      controller: "joinController",
+      controllerAs: "join"
     });
   }
 ]);
 
 app.controller('loginpageController', function() {
+     vm = this;
+   }
+ );
+
+app.controller('joinController', function() {
      vm = this;
    }
  );
